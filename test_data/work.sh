91a864f67bbe60ebff5425b6a39b611ed3e8b382
Xiongliwen <71487119+wen1112@users.noreply.github.com>
2024-05-31 16:57:37 +0800

    Update work.sh

--- test_data/work.sh
+++ test_data/work.sh
@@ -2,4 +2,4 @@ conda activate VCP_env
 
 snakemake -s /hwfsxx1/ST_HN/P17Z10200N0246/User/xiongliwen/QuantifyPhageProject/snakemake/00.VCP_smk_V1.0/Snakefile.py \
 --configfile /hwfsxx1/ST_HN/P17Z10200N0246/User/xiongliwen/QuantifyPhageProject/snakemake/00.VCP_smk_V1.0/test_data/config.yaml \
---jobs 99 --cores 1 --max-threads 16
+--jobs 99 --cores 8
